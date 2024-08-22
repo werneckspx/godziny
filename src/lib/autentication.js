00431604cd6809e07a82668ae2f8a113c45e1342
teuswx <mateushenriqpereira02@gmail.com>
2024-08-22 12:48:48 -0300

    integração completa

--- src/lib/autentication.js
+++ src/lib/autentication.js
@@ -1,22 +1,26 @@
 import axios from "axios";
 
 const loginGeral = {}
-export default {
-    async autenticacaoGlobal(email, senha) {  
-        try {
-            const loginDTO = {
-                email: email,
-                senha: senha
-            };
-            const response = await axios.post("http://localhost:8080/auth/login", loginDTO);
-            loginGeral.data = response.data;
-            loginGeral.status = response.status;
-            loginGeral.headers = response.headers;
-        } catch (error) {
-            console.error("Erro ao buscar dados:", error);
-            throw error; // Lança o erro para ser tratado por quem chamar o método
-        }
 
-        console.log(loginGeral)
+async function autenticacaoGlobal(email, senha) {  
+    try {
+        const loginDTO = {
+            email: email,
+            senha: senha
+        };
+        const response = await axios.post("http://localhost:8080/auth/login", loginDTO);
+        loginGeral.data = response.data;
+        loginGeral.status = response.status;
+        loginGeral.headers = response.headers;
+    } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+        throw error; // Lança o erro para ser tratado por quem chamar o método
     }
+
+    console.log(loginGeral)
+}
+
+export default {
+    loginGeral,
+    autenticacaoGlobal
 }
